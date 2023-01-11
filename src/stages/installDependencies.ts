9792901c334b72f33acd26d09d743173df18006c
Micah Ernst <micahwave@gmail.com>
2023-01-11 09:26:07 -0500

    testing npm ci

--- src/stages/installDependencies.ts
+++ src/stages/installDependencies.ts
@@ -11,7 +11,7 @@ export const installDependencies = async (
     // NOTE: The `npm ci` command is not used. Because if your version of npm is old, the generated `package-lock.json` will also be old, and the latest version of `npm ci` will fail.
     await removeDirectory(joinPaths(workingDirectory, 'node_modules'));
 
-    await exec(`${packageManager} install`, undefined, {
+    await exec(`${packageManager} ci --ignore-scripts`, undefined, {
         cwd: workingDirectory,
     });
 };
