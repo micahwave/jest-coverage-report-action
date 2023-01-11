6526e708ded686cd8e9ccbc6a73e82b9116ea5e2
Micah Ernst <micahwave@gmail.com>
2023-01-11 10:21:44 -0500

    remove flag

--- src/stages/installDependencies.ts
+++ src/stages/installDependencies.ts
@@ -19,7 +19,7 @@ export const installDependencies = async (
         cwd: workingDirectory,
     });
 
-    await exec(`${packageManager} ci --ignore-scripts`, undefined, {
+    await exec(`${packageManager} ci`, undefined, {
         cwd: workingDirectory,
     });
 };
